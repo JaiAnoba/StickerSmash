f39159aae53e790a7a47f70f26f828120d197083
JaiAnoba <kaeanver@gmail.com>
2025-06-25 22:28:52 +0800

    added env for backserver

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,9 +6,9 @@ let API_URL = '';
 if (Platform.OS === 'web') {
   API_URL = Constants.manifest?.extra?.API_URL || process.env.API_URL || 'http://localhost:8000';
 } else if (Platform.OS === 'android') {
-  API_URL = Constants.manifest?.extra?.API_URL || process.env.API_URL || 'http://192.168.254.111:8000';
+  API_URL = Constants.manifest?.extra?.API_URL || process.env.API_URL || 'http://192.168.100.38:8000';
 } else {
-  API_URL = Constants.manifest?.extra?.API_URL || process.env.API_URL || 'http://192.168.254.111:8000';
+  API_URL = Constants.manifest?.extra?.API_URL || process.env.API_URL || 'http://192.168.100.38:8000';
 }
 
 export { API_URL };
